416b95002bf3dc0db51eb35c0671f6db18a280ea
ZKasmi <zakaria@zenithblox.network>
2026-02-23 23:09:01 +0000

    testing fix

--- api/hands/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { ReplayPage } from "./ReplayPage.js";
-export { Header } from "./Header.js";
-export { TableContainer } from "./TableContainer.js";
-export { Controls } from "./Controls.js";
-export { WinOverlay } from "./WinOverlay.js";
-export { CtaOverlay } from "./CtaOverlay.js";
